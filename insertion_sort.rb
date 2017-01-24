707a5bdec954038d1d0854f394f994e226d4970d
gazayas <g-zayas@hotmail.com>
2017-01-24 11:22:40 +0900

    insertion_sortが出来た！！

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -1,7 +1,22 @@
 require 'variables.rb'
 
-def insertion_sort ary
+def insertion_sort ary  
   ary.each do |value|
-    
+    ary.index(value).downto(0) do |n|
+      if value == ary[n]
+        next
+      elsif value > ary[n]
+        break
+      else # value < ary[n]
+        ary[n + 1] = ary[n]
+        ary[n] = value
+        if value < ary[n - 1]
+          next
+        else
+          break
+        end
+      end
+    end
   end
+  ary
 end
