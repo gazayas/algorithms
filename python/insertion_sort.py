56eee305e50bdd7c1a7736570fc3a0f546bdedfd
gazayas <g-zayas@hotmail.com>
2017-02-15 19:44:14 +0900

    Pythonで挿入ソート

--- python/insertion_sort.py
+++ python/insertion_sort.py
@@ -1,22 +1,24 @@
-# unfinished
-
 def insertion_sort(ary):
   for val in ary:
-    rng = range(len(ary) -1 , -1, -1) # count backwards
+    rng = range(ary.index(val), -1, -1)
+    # print(rng) Check the contents of rng
     for i in rng:
       if val == ary[i]:
         continue
       elif val > ary[i]:
         break
       else:
-        #ary[i + 1] = ary[i]
-        #ary[i] = val
-        if val < ary[i - 1]:
-          continue
-        else:
-          break
+        if i + 1 < len(ary):
+          ary[i + 1], ary[i] = ary[i], val
+          # print(ary) Check the process
+          if i - 1 > 0:
+            if val < ary[i - 1]:
+              continue
+            else:
+              break
+  return ary
 
-  print ary
 
-
-insertion_sort([2, 1, 3, 7, 5, 6, 4])
+# Make this into a test
+ary = insertion_sort([2, 1, 3, 7, 5, 6, 4])
+print(ary)
