7c4a507f90efbafaaa53abaa390a59b161f1ee1f
Gabriel Zayas <g-zayas@hotmail.com>
2017-02-16 20:17:40 +0900

    bubble_sortのテストの追加

--- python/test_algorithms.py
+++ python/test_algorithms.py
@@ -1,7 +1,11 @@
 from insertion_sort import insertion_sort
+from bubble_sort import bubble_sort
 
 ary        = [2, 1, 3, 7, 5, 6, 4]
 sorted_ary = [1, 2, 3, 4, 5, 6, 7]
 
 def test_insertion_sort():
   assert insertion_sort(ary) == sorted_ary
+
+def test_bubble_sort():
+  assert bubble_sort(ary) == sorted_ary
